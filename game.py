130bbf71dd5be309dd25393c1dec3fe45dcee0b8
Daniel <todanielhy@gmail.com>
2020-08-29 12:32:15 +1000

    Gravity added, Created Obstacle class

--- game.py
+++ game.py
@@ -12,12 +12,16 @@ width = 800
 height = 1200
 gravity = 5
 
-# Initializing pygame and images
+# Initializing pygame 
 vec = pygame.math.Vector2
-
 pygame.init()
 gameScreen = pygame.display.set_mode((width,height))
+
+# Loading Sprites
 playerImg = pygame.image.load('Block.png').convert()
+obstacleImg = pygame.Surface((50,800))
+obstacleImg.fill(GREEN)
+
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 
@@ -34,12 +38,14 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (50,45))
         self.rect = self.image.get_rect()
         self.rect.center = (200,600)
-        self.vel = -1
+        self.vel = -10
         self.isJump = False
-        self.jumpCount = 10
+        self.jumpCount = 6
     
     def update(self):
         keys = pygame.key.get_pressed()
+
+        # Jump Mechanic
         if not self.isJump:
             if keys[pygame.K_SPACE]:
                 self.isJump = True
@@ -50,15 +56,26 @@ class Player(pygame.sprite.Sprite):
                 self.rect.move_ip(0,self.vel) 
             else:
                 self.isJump = False 
-                self.jumpCount = 10
-            
+                self.jumpCount = 6
 
+        # Still need to build in Acceleration of gravity on the player.
+        self.rect.move_ip(0,gravity)
+            
 # Obstacle Class
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = obstacleImg
+        self.rect = self.image.get_rect()
+        self.rect.center = (500,0)
+
 
 
 # Sprites to load
 player = Player()
 player.add(all_sprites)
+obs = Obstacle()
+obs.add(all_sprites)
 
 # Main Game Loop
 while not gameOver:
