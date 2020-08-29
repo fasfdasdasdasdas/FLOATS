198954d148a85edb1529b062fc886220f37a892e
annasu4 <70382158+annasu4@users.noreply.github.com>
2020-08-29 20:11:05 +1000

    Update game.py

--- game.py
+++ game.py
@@ -2,18 +2,27 @@ import pygame
 import random
 from items_class import Mask, Vaccine, Player, Enemy
 
-#initalise 
+#initialisation 
 pygame.init()
-
 screen  = pygame.display.set_mode((1000, 800))
 
 #caption + icon
-background_image = pygame.image.load("12-1000 x 800.jpg")
+scoreboardIMG = pygame.image.load("111-400 x 400.png") #leaderboard
+#background_image = pygame.image.load("12-1000 x 800.jpg")
+background_image = pygame.image.load("panic_mode_.gif")
+
 pygame.display.set_caption("COVID GAME")
-background_image = pygame.image.load("12-1000 x 800.jpg").convert()
+background_image = pygame.image.load("panic_mode_.gif").convert()
+textImg  = pygame.image.load("yay-30 x 30.PNG")
 
 playerImg = pygame.image.load("coronavirus-70 x 70.png")
-enemyImg = pygame.image.load("toilet paper-40 x 40.PNG")
+enemyImg = pygame.image.load("person-55 x 55.png") 
+
+#adding some background music ~
+
+pygame.mixer.music.load("helloo.wav")
+pygame.mixer.music.play(-1)
+
 
 enemyY = [600, 600, 600, 600, 600]
 enemyX = []
@@ -46,14 +55,14 @@ def enemy(x, y): #draw
 
 def redrawGameWindow(): #change drawing 
     screen.blit(background_image, (0,0))
-   # player.draw(screen)
-
+    screen.blit(scoreboardIMG, (-10, -80))
+    
 while running:
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False #exit
-
+    
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and playerx > 2: # + boundary
         playerx -=2
@@ -79,18 +88,18 @@ while running:
                 negative = -1
 
             playery -= (jumpCount **2 ) *0.5 * negative #move down a little
-           # player.x -= jumpCount *0.5 * negative 
             jumpCount -=1 #slowly move down
     
         else: #jump ends
             isJump = False  #can jump up/down again
             jumpCount = 10 #reset 
     
-    #screen.blit(background_image, (0,0))
-    screen.fill((0,220, 10)) #fill screen before player shows up
+    screen.fill((0,250, 60)) #fill screen before player shows up
     redrawGameWindow()
-
+    #player = Player(250, 380)
+    #player_show(player.x, player.y)
     player.player1(playerx, playery)
+
     vaccine2 = Vaccine()
     mask2 = Mask()
     mask2.mask_anim(xm, ym)
@@ -99,20 +108,19 @@ while running:
     if a: # true
         mask2.y = player.y
         score +=1
+        player.player_move(player.x, player.y)
         print(score)
-
+        
     vaccine2.vaccine(v1, v2)
-
     vaccine2.end_game(player)
     
-    #having multiple enemies aka humans
     
     for i in range(5):
         enemyX.append(random.randint(0, 830))
         #enemyY.append(random.randint(50, 150))
         enemyX_change.append(4)
         enemyY_change.append(40)
-   
+    
     #enemy movement 
     
     for i in range(5):
@@ -124,10 +132,8 @@ while running:
         elif enemyX[i] >= 900:
             enemyX_change[i] = -3
             enemyY[i] += enemyY_change[i]
-  
+            
     enemyX += enemyX_change
-
-    #screen.blit(background_image, [0,0])
     pygame.display.update() #shows player on screen
     
 
